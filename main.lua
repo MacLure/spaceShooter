6caa645392966e0124ee59cdbc23d1f12f150bed
Malcolm <malcolm.maclure@gmail.com>
2019-04-05 19:34:40 -0400

    fixed errors

--- main.lua
+++ main.lua
@@ -1,14 +1,14 @@
 WINDOW_WIDTH = 1280
-WINDOW_HEIHGT = 720
+WINDOW_HEIGHT = 720
 
 VIRTUAL_WIDTH = 512
 VIRTUAL_HEIGHT = 288
 
 require 'src/Dependencies'
 
-function lua.load()
+function love.load()
   love.graphics.setDefaultFilter('nearest', 'nearest')
-  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT {
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
     vsync = true,
     fullscreen = false,
     resizable = true
