3531acc34e79bc8736459b6c136b84231847566d
Malcolm <malcolm.maclure@gmail.com>
2019-04-05 19:31:02 -0400

    push screen filter

--- main.lua
+++ main.lua
@@ -7,4 +7,11 @@ VIRTUAL_HEIGHT = 288
 require 'src/Dependencies'
 
 function lua.load()
+  love.graphics.setDefaultFilter('nearest', 'nearest')
+  push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT {
+    vsync = true,
+    fullscreen = false,
+    resizable = true
+  })
+  math.randomseed(os.time())
 end
